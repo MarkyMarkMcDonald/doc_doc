aac8d4af6342e79d47debcfdd477a983b96e3b6e
Mark McDonald <mark.mcdonald.ga@gmail.com>
2018-03-17 18:17:45 -0400

    Fix whitespace

--- lib/doc_doc/house_visit.rb
+++ lib/doc_doc/house_visit.rb
@@ -1,41 +1,41 @@
-   module DocDoc
-     class HouseVisit
-       def initialize(horse_and_buggy, patient, starting_location)
-         @horse_and_buggy = horse_and_buggy
-         @patient = patient
-         @starting_location = starting_location
-       end
+module DocDoc
+  class HouseVisit
+    def initialize(horse_and_buggy, patient, starting_location)
+      @horse_and_buggy = horse_and_buggy
+      @patient = patient
+      @starting_location = starting_location
+    end
 
-       attr_reader :starting_location
+    attr_reader :starting_location
 
-       def start
-         @horse_and_buggy.visit_house(@patient.home)
-         if @horse_and_buggy.status_code >= 400
-           @illness = OpenStruct.new(type: 'http', status: @horse_and_buggy.status_code)
-         end
-       rescue Capybara::Poltergeist::StatusFailError
-         @illness = OpenStruct.new(type: 'http', description: 'Could not reach server')
-       end
+    def start
+      @horse_and_buggy.visit_house(@patient.home)
+      if @horse_and_buggy.status_code >= 400
+        @illness = OpenStruct.new(type: 'http', status: @horse_and_buggy.status_code)
+      end
+    rescue Capybara::Poltergeist::StatusFailError
+      @illness = OpenStruct.new(type: 'http', description: 'Could not reach server')
+    end
 
-       def illness
-         @illness ||= begin
-           if @patient.home && @patient.home.match(/#([^#]+)$/)
-             hyper_fragment = @patient.home.split('#').last
-             begin
-               nil if @horse_and_buggy.find("[id=\"#{hyper_fragment}\"]")
-             rescue Capybara::Ambiguous
-               OpenStruct.new(type: 'fragment', description: 'More than one dom node has this id')
-             rescue Capybara::ElementNotFound
-               OpenStruct.new(type: 'fragment', description: 'No dom node with this id found')
-             rescue StandardError => e
-               if $DEBUG
-                 puts "[DOC_DOC ERROR] Ran into exception when trying to follow: #{@patient.home}\n#{e.message}"
-               end
-             end
-           else
-             nil
-           end
-         end
-       end
-     end
-   end
+    def illness
+      @illness ||= begin
+        if @patient.home && @patient.home.match(/#([^#]+)$/)
+          hyper_fragment = @patient.home.split('#').last
+          begin
+            nil if @horse_and_buggy.find("[id=\"#{hyper_fragment}\"]")
+          rescue Capybara::Ambiguous
+            OpenStruct.new(type: 'fragment', description: 'More than one dom node has this id')
+          rescue Capybara::ElementNotFound
+            OpenStruct.new(type: 'fragment', description: 'No dom node with this id found')
+          rescue StandardError => e
+            if $DEBUG
+              puts "[DOC_DOC ERROR] Ran into exception when trying to follow: #{@patient.home}\n#{e.message}"
+            end
+          end
+        else
+          nil
+        end
+      end
+    end
+  end
+end
