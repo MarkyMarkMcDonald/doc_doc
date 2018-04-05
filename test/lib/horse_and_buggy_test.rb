87ccc6722add7dd724fdacc2ae0d21faf41bb94a
Mark McDonald <mark.mcdonald.ga@gmail.com>
2018-04-05 12:35:48 -0400

    Shore up drifting tests
    Whoops, that's embarrassing

--- test/lib/horse_and_buggy_test.rb
+++ test/lib/horse_and_buggy_test.rb
@@ -4,22 +4,22 @@ module DocDoc
   class HorseAndBuggyTest < ::Minitest::Test
     def test_road_is_closed_eh_for_connection_error
       horse_and_buggy = HorseAndBuggy.new(0)
-      assert(horse_and_buggy.road_closed?('http://will-never-exist.example'))
+      assert(horse_and_buggy.road_closure('http://will-never-exist.example'))
     end
 
     def test_road_is_closed_eh_for_page_not_found
       horse_and_buggy = HorseAndBuggy.new(0)
-      assert(horse_and_buggy.road_closed?('https://github.com/markymarkmcdonald/project-does-not-exist'))
+      assert(horse_and_buggy.road_closure('https://github.com/markymarkmcdonald/project-does-not-exist'))
     end
 
     def test_road_is_closed_eh_for_open_road
       horse_and_buggy = HorseAndBuggy.new(0)
-      assert(!horse_and_buggy.road_closed?('https://google.com'))
+      assert(!horse_and_buggy.road_closure('https://google.com'))
     end
 
     def test_road_is_closed_eh_for_winding_closed_road
       horse_and_buggy = HorseAndBuggy.new(0)
-      assert(horse_and_buggy.road_closed?('http://www.github.com/markymarkmcdonald/redirect-to-nonexistant-project'))
+      assert(horse_and_buggy.road_closure('http://www.github.com/markymarkmcdonald/redirect-to-nonexistant-project'))
     end
   end
 end
